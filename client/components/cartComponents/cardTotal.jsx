710298def0a24912b560e1acb54ffcfe50b74de2
duplixx <duplixxx20@gmail.com>
2023-08-25 04:01:09 +0530

    cartdetails 1

--- client/components/cartComponents/cardTotal.jsx
+++ client/components/cartComponents/cardTotal.jsx
@@ -9,59 +9,57 @@ import { Box } from '@mui/material';
 
 function CardTotal({cartTotal,items,nextStep,prevStep}) {
   return (
-    <Card className={`${nova.className} md:fixed md:right-0 md:mr-[50px] md:m-[20px] md:w-[20%] md:h-[40%] `}>
-      <CardContent className="p-0">
-      <Box className="p-2">
-        <Typography variant='h6' className="border-b-2 mb-2 mt-1">
-          Payment Details
-        </Typography>
+    <div className={`${nova_thai.className} p-4 md:p-0 bg-white md:fixed md:right-0 md:mr-10 md:mt-5 md:w-1/5 md:h-2/5 rounded-md shadow-lg`}>
+      <div className="p-0">
+        <div className="p-2">
+          <h6 className="border-b-2 mb-2 mt-1 text-lg font-semibold">
+            Payment Details
+          </h6>
 
-        <div className='flex justify-between mt-1 '>
-          <Typography className="text-black text-sm font-semibold">
-            Price ({items.length} items)
-          </Typography>
-          <Typography className="text-black text-sm font-medium">
-            ₹ {cartTotal}            
-          </Typography>
+          <div className="flex justify-between mt-1">
+            <p className="text-black text-sm font-semibold">
+              Price ({items.length} items)
+            </p>
+            <p className="text-black text-sm font-medium">
+              ₹ {cartTotal}
+            </p>
+          </div>
+
+          <div className="flex justify-between mt-1 border-b-2 mb-2">
+            <p className="text-black text-sm font-semibold mb-1">
+              Delivery Charges
+            </p>
+            <p className="text-green-600 text-sm font-medium">
+              FREE
+            </p>
+          </div>
+
+          <div className="flex justify-between mt-2">
+            <p className="text-black text-sm font-semibold">
+              Total Amount
+            </p>
+            <p className="text-black text-sm font-medium">
+              ₹ {cartTotal}
+            </p>
+          </div>
         </div>
 
-        <div className='flex justify-between mt-1 border-b-2 mb-2'>
-          <Typography className="text-black text-sm font-semibold mb-1">
-            Delivery Charges
-          </Typography>
-          <Typography className="text-green-600 text-sm font-medium">
-            FREE
-          </Typography>
-        </div>
-
-        <div className='flex justify-between mt-2 '>
-          <Typography className="text-black text-sm font-semibold">
-            Total Amount
-          </Typography>
-          <Typography className="text-black text-sm font-medium">
-          ₹ {cartTotal}  
-          </Typography>
-        </div>
-        </Box>
-        
-<div className='flex justify-between py-2 bg-green-200 mt-1 px-2'>
-          <Typography className="text-green-600 text-sm font-semibold">
+        <div className="flex justify-between py-2 bg-green-200 mt-1 px-2">
+          <p className="text-green-600 text-sm font-semibold">
             You will save ₹ 76,000 on this order
-          </Typography>
+          </p>
         </div>
-        <Box className="px-4">
-        <Button
-          variant="contained"
-          endIcon={<ArrowRightAltIcon />}
-          className="w-full mt-6 p-4 rounded-md bg-green-600 hover:bg-green-700 text-lg px-4"
-          onClick={nextStep}
-        >
-          PROCEED
-        </Button>
-        </Box>
-      </CardContent>
-      
-    </Card>
+
+        <div className={`${nova.className} px-4`}>
+          <button
+            className="w-full mt-6 p-4 rounded-md bg-green-500 hover:bg-green-700 text-lg px-4 text-white"
+            onClick={nextStep}
+          >
+            PROCEED
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
